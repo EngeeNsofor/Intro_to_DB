dd52fe51c53f2ed5ac71b643d1ae5e5f920deb39
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-10-20 21:04:35 +0100

    Updated task_6.sql file

--- task_6.sql
+++ task_6.sql
@@ -30,8 +30,8 @@ try:
     """
     customers_data = [
         (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
-        (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-        (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.')
+        (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+        (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.')
     ]
     
     mycursor.executemany(sql, customers_data)
